bcf7b3be3be8393d334a6684058d2dd6bff08e59
yuchannn <yuchanbpp@gmail.com>
2022-02-24 22:21:03 +0900

    typescript - state and forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import React, { useState } from "react";
 import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
   return (
     <div>
-      <Circle borderColor="black" bgColor="tomato"/>
-      <Circle text="I'm here" bgColor="teal"/>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
