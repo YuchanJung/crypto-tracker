50aa331d218b0ce20191452f5ca1c10725c556ce
yuchannn <yuchanbpp@gmail.com>
2022-02-25 22:35:44 +0900

    Crypto Tracker App Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.div`
-  color: ${(props) => props.theme.textColor};
-`;
-
+import Router from "./Router";
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-      <Container>
-        <H1>protected</H1>
-      </Container>
-    </div>
+    <Router />
   );
 }
 
