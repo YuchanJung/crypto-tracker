91eb2902c89522e8961e12575fb0404ee8b8aff8
yuchannn <yuchanbpp@gmail.com>
2022-02-26 23:01:39 +0900

    Crypto Tracke - basic frame

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,11 +1,89 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Containter = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  /*padding: 20px; */
+  margin-bottom: 10px;
+  border-radius: 15px;
+  a {
+    padding: 20px; /* 패딩 위치를 어디에 넣냐에 따라 링크 클릭 범위가 달라짐 */
+    transition: color 0.2s ease-in;
+    display: block; /* 글씨 밖까지 클릭되게 함 */
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "usdt-tether",
+    name: "Tether",
+    symbol: "USDT",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
 function Coins() {
-  return <Title>Coins</Title>;
+  return (
+    <Containter>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      <CoinsList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinsList>
+    </Containter>
+  );
 }
 
 export default Coins;
