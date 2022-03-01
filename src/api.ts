52d8a707f278a3859d68681f6e8950f1b5d408ba
yuchannn <yuchanbpp@gmail.com>
2022-03-01 23:39:23 +0900

    Crypto Tracker - fetch CoinInfo, CoinTickers

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,17 @@
+const BASE_URL = "https://api.coinpaprika.com/v1";
+
 export function fetchCoins() {
-  return fetch(`https://api.coinpaprika.com/v1/coins`).then((response) =>
+  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
+}
+
+export function fetchCoinInfo(coinID: string) {
+  return fetch(`${BASE_URL}/coins/${coinID}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinTickers(coinID: string) {
+  return fetch(`${BASE_URL}/tickers/${coinID}`).then((response) =>
     response.json()
   );
 }
