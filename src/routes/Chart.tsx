b7b913102881011b9dc42c1aaedc05f5c93b1d31
yuchannn <yuchanbpp@gmail.com>
2022-03-02 13:57:02 +0900

    Crypto Tracker - showing chart with ApexChart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,73 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
-interface IChart {
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+interface ChartProps {
   coinID: string;
 }
 
-function Chart({ coinID }: IChart) {
-  const { isLoading, data } = useQuery(["ohlcv", coinID], () =>
+function Chart({ coinID }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinID], () =>
     fetchCoinHistory(coinID)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading Charts..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "sales",
+              data: data?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              background: "transparent",
+              height: 500,
+              width: 500,
+              dropShadow: { enabled: true },
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            xaxis: {
+              labels: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              axisBorder: {
+                show: false,
+              },
+            },
+            yaxis: {
+              show: false,
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
